6213f628ba13af31e20aa08c921ef1467f5b6349
kashc <kashc@THE_BEAST>
2026-09-05 14:49:00 +0200

    set up the basiis for the local database. installed relevant nuget packages

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using My_Study_App.Models;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace My_Study_App.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Username)
+                .IsUnique();
+
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+        }
+    }
+}
---
+++ Models/AccountViewModels.cs
@@ -0,0 +1,35 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace My_Study_App.Models
+{
+    public class AccountViewModels
+    {
+
+        [Required]
+        [MaxLength(50)]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage = "Passwords do not match. Try again")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class loginViewModel {
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public String Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    
+    }
+}
