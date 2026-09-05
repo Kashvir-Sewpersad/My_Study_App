efae3f152f260803ba64156533d3f86085cfe1c0
kashc <kashc@THE_BEAST>
2026-09-05 12:21:49 +0200

    Inital push to confirm connection to github

---
+++ Controllers/SubjectController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace My_Study_App.Controllers
+{
+    public class SubjectController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
