76b1faafffb0d3c8c5f11455919cb742ca48beda
kashc <kashc@THE_BEAST>
2026-09-05 12:46:04 +0200

    Setting up the accessors for the login system with required fields

---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace My_Study_App.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Username { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Email { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+    }
+}
